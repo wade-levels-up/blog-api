cdfddacecb4e0908c0c0ea765e9027ffe266b731
Wade Foster <172018441+wade-levels-up@users.noreply.github.com>
2025-03-09 18:48:39 +1100

    Move business logic from route to controller

--- app.js
+++ app.js
@@ -6,8 +6,12 @@ const routes = require("./routes/index");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/", routes.indexRouter);
 app.use("/user", routes.userRouter);
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.statusCode || 500).json({ error: err.message });
+});
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server live at: http://localhost:${PORT}`));
--- routes/user.js
+++ routes/user.js
@@ -1,16 +1,7 @@
 const { Router } = require("express");
 const userRouter = Router();
-const asyncHandler = require("express-async-handler");
-const database = require("../services/databaseService");
+const { addUser } = require("../controllers/userController");
 
-userRouter.post(
-  "/",
-  asyncHandler(async (req, res) => {
-    await database.addUser(req.body.username, req.body.password);
-    res
-      .status(200)
-      .json({ message: `Added ${req.body.username} to the database` });
-  })
-);
+userRouter.post("/", addUser);
 
 module.exports = { userRouter };
