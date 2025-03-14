4f1cad3d58c4462dd3c79b57e876882237e00747
Wade Foster <172018441+wade-levels-up@users.noreply.github.com>
2025-03-14 17:26:15 +1100

    Implement simple usage for CORS

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const passport = require("./services/authService");
 const app = express();
 const routes = require("./routes/index");
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
