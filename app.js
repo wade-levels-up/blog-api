539f67a5a54e8e11d265dfdcb395dcedc832e054
Wade Foster <172018441+wade-levels-up@users.noreply.github.com>
2025-03-24 17:33:12 +1100

    Add client url to cors settings

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ const passport = require("./services/authService");
 const app = express();
 const routes = require("./routes/index");
 
-app.use(cors());
+const corsOptions = {
+  origin: process.env.CLIENT_URL,
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
