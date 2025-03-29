8db25fa17b5d328b2eeb3339d3a044520edf6be2
Wade Foster <172018441+wade-levels-up@users.noreply.github.com>
2025-03-29 14:45:30 +1100

    Update cors error message

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const corsOptions = {
     if (allowedOrigins.includes(origin) || !origin) {
       callback(null, true);
     } else {
-      callback(new Error("Not allowed by CORS"));
+      callback(new Error("Not allowed by CORS @API"));
     }
   },
   optionsSuccessStatus: 200,
